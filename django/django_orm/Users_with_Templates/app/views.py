52388d9e1cde6b6cdb3dec7449d5442de2c85960
MohammedAbuMukh <mohammedabomokh3@gmail.com>
2024-11-09 22:22:26 +0200

    add django_orm & Ninja Gold

---
+++ django/django_orm/Users_with_Templates/app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .models import User
+
+def show_users(request):
+    users = User.objects.all()
+    return render(request, 'index.html',{'users':users})
+
+
+def add_user(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email_address = request.POST['email_address']
+        age = request.POST['age']
+        
+        User.objects.create(first_name=first_name, last_name=last_name, email_address=email_address, age=age)
+        return redirect('/')
